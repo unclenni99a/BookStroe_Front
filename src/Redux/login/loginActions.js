2ec3c87d0882b4cc491578492dbcf6f921da6c47
AFRAAD123 <tony@local.com>
2021-08-25 14:01:37 +0200

    Login sent request done

--- src/Redux/login/loginActions.js
+++ src/Redux/login/loginActions.js
@@ -3,19 +3,20 @@ import { LOGIN_FAILED, LOGIN_SUCCEED } from './loginTypes'
 
 export const login = (username, password) => {
     return dispatch => {
+        console.log(username + '  ' + password)
         const headers = {
             'Content-Type' : 'application/json',
             Authorization: username + ';' + password
         }
-        axios.post("/login","",headers)
+        axios.post("login","",headers)
         .then(response => {
             console.log(response.data)
             dispatch({type:LOGIN_SUCCEED, payload: response.data})
         })
         .catch(error => 
         {
-            console.log(error.data)
-            dispatch({type:LOGIN_FAILED, payload: error.data})
+            console.log(error)
+            dispatch({type:LOGIN_FAILED})
         })
     }
 }
