1a2e982dd112fb0f8d09417d2293e7b0194ed590
AFRAAD123 <tony@local.com>
2021-08-26 08:37:13 +0200

    Finalized login connection with remote logic

--- src/Redux/login/loginActions.js
+++ src/Redux/login/loginActions.js
@@ -3,12 +3,13 @@ import { LOGIN_FAILED, LOGIN_SUCCEED } from './loginTypes'
 
 export const login = (username, password) => {
     return dispatch => {
+        const article = { title: 'Login Request' };
         console.log(username + '  ' + password)
         const headers = {
             'Content-Type' : 'application/json',
-            Authorization: username + ';' + password
+            Authorization: username + ';' + password,
         }
-        axios.post("login","",headers)
+        axios.post("login", article, {headers})
         .then(response => {
             console.log(response.data)
             dispatch({type:LOGIN_SUCCEED, payload: response.data})
